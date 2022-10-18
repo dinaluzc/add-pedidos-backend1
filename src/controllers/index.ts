df7c270f70d4f895b91468e59be646b8fe6e36b5
dinaluzc <dinaluz@gmail.com>
2022-10-17 20:16:59 -0500

    version 1.0.0. de la aplicacion de pedidos

---
+++ src/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from './ping.controller';
+export * from './pedido-persona.controller';
+export * from './persona-pedido.controller';
+export * from './pedido-producto.controller';
+export * from './personas.controller';
+export * from './producto.controller';
+export * from './pedido.controller';
